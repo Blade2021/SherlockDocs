2d15febc9c9a7c974513c07ed76ed78bf33097e8
Matt <Blade2021@live.com>
2022-01-20 18:06:09 -0500

    Changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Sherlock Docs',
+  tagline: 'Hello!  I am Sherlock',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Blade2021', // Usually your GitHub org/user name.
+  projectName: 'Sherlock', // Usually your repo name.
 
   presets: [
     [
